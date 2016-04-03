09ced029d8480b4f8543a6110c09369f21b7021a
anna tolen <atolen@stuy.edu>
2016-04-03 16:16:50 -0400

    updates

--- LLStack.java
+++ LLStack.java
@@ -1,24 +1,59 @@
-/*****************************************************
- * class LLStack
- * Implements ADT stack as specified in local Stack.java
- * Generically typed
- * Uses a LinkedList as underlying container
- *****************************************************/
+/* Team ??? -- Elias Milborn, Anna Tolen
+APCS2 pd9
+HW23 -- Leon Leonwood Stack
+2016-04-02 */
 
 import java.util.LinkedList;
 
-
 public class LLStack<T> implements Stack<T> {
 
+    private LinkedList<T> _stack;
+    private int _stackSize;
 
+    //constructor
+    public LLStack() {
+	_stack = new LinkedList<T>();
+	_stackSize = 0;
+    }
 
+    //chk for fullness
+    public boolean isFull() {
+	return _stackSize == _stack.size();
+    }
 
+    //Stack interface methods
+    
+    public boolean isEmpty() {
+	return _stackSize == 0;
+    }
+
+    public T peek() {
+	if( isEmpty() ) { return null; }
+	return _stack.get(_stackSize-1);
+    }
+
+    public T pop() {
+	if( isEmpty() ) { return null; }
+
+	T tmp = _stack.get(_stackSize-1);
+	_stack.remove(_stackSize-1);
+	_stackSize--;
+	return tmp;
+    }
+
+    public void push( T x ) {
+	_stack.add(x);
+	_stackSize++;
+    }
+
+    public String toString() { return _stack.toString(); }
     //main method for testing
     public static void main( String[] args ) {
 
-	Stack<String> tastyStack = new LLStack<String>();
+	LLStack<String> tastyStack = new LLStack<String>();
 
 	tastyStack.push("aoo");
+	
 	tastyStack.push("boo");
 	tastyStack.push("coo");
 	tastyStack.push("doo");
@@ -67,11 +102,12 @@ public class LLStack<T> implements Stack<T> {
 	//aoo
 	System.out.println( "peek: " + tastyStack.peek() );
 	System.out.println( "pop: " + tastyStack.pop() );
-
 	//stack empty by now; SOP(null)
 	System.out.println( "peek: " + tastyStack.peek() );
 	System.out.println( "pop: " + tastyStack.pop() );
 	System.out.println( tastyStack.pop() );
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+          ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
     }//end main()
-
-}//end class LLStack
+}
+    
